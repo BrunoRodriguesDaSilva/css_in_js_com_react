af5f077c78edafdcd3d01805911aa8642250898a
Bruno Rodrigues da Silva <Brunochh@outlook.com>
2023-10-31 22:24:52 -0300

    Criação dos temas Ligth e Dark e termino do Módulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import EstiloGLobal, { Container } from './styles'
 import Sidebar from './container/Sidebar'
 import Sobre from './container/Sobre'
 import Projetos from './container/Projetos'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [themeDark, setThemeDark] = useState(false)
+
+  function trocaTema() {
+    setThemeDark(!themeDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={themeDark ? temaDark : temaLight}>
       <EstiloGLobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Titulo/styles.ts
+++ src/components/Titulo/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import { Props } from '.'
 
 export const Titulo = styled.h3<Props>`
-color= #282a35;
-font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
-font-weight: bold;
-margin-bottom: 16px;
+  color: ${(props) => props.theme.corPrincipal};
+  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
+  font-weight: bold;
+  margin-bottom: 16px;
 `
